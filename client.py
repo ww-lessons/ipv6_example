dcbf17718aeba705529bb06e8f80808281f3aba5
Wolfgang Wiedermann <wolfgang.wiedermann@hs-regensburg.de>
2018-01-30 22:01:50 +0100

    Umgebaut in IPv6 über localhost ::1

--- client.py
+++ client.py
@@ -3,11 +3,11 @@
 
 import socket
 
-HOST = 'localhost'    # The remote host
+HOST = '::1'    # The remote host
 PORT = 50007              # The same port as used by the server
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+with socket.socket(socket.AF_INET6, socket.SOCK_STREAM, 0) as s:
     s.connect((HOST, PORT))
     s.sendall(b'Hello, world')
     data = s.recv(1024)
-print('Received', repr(data))
+print('Received: {0}'.format(data))
 
--- server.py
+++ server.py
@@ -5,10 +5,10 @@ import socket
 
 # Quelle: https://docs.python.org/3/library/socket.html#example
 
-HOST = ''
+HOST = '::1'
 PORT = 50007
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+with socket.socket(socket.AF_INET6, socket.SOCK_STREAM, 0) as s:
     s.bind((HOST, PORT))
     s.listen(1)
     con, addr = s.accept()
